604745459746fbb3c42d5fd5d69aa96aa76afb24
Matt Hicks <matt@outr.com>
2018-07-04 10:28:15 -0500

    Fixes and breakages

--- shared/src/main/scala/reactify/State.scala
+++ shared/src/main/scala/reactify/State.scala
@@ -23,11 +23,23 @@ case class State[T](owner: Reactive[T], function: () => T) {
     }
     val modified = _value != value
     _value = value
+    val removed = _references.diff(references)
+    val added = references.diff(_references)
+    removed.foreach(removeReference)
+    added.foreach(addReference)
     _references = references
     if (modified) Reactive.fire(owner, value)
   }
 
+  private def addReference(state: State[_]): Unit = {
+    state.owner.asInstanceOf[Reactive[Any]].reactions += reaction
+  }
+
+  private def removeReference(state: State[_]): Unit = {
+    state.owner.asInstanceOf[Reactive[Any]].reactions -= reaction
+  }
+
   def clearReferences(): Unit = synchronized {
-    references.foreach(_.owner.asInstanceOf[Reactive[Any]].reactions -= reaction)
+    references.foreach(removeReference)
   }
 }
--- shared/src/test/scala/test/BasicVarSpec.scala
+++ shared/src/test/scala/test/BasicVarSpec.scala
@@ -12,7 +12,9 @@ class BasicVarSpec extends WordSpec with Matchers {
     "see a simple change" in {
       var changed = 0
       val v = Var("Hello")
+      v.reactions().size should be(0)
       v.on(changed += 1)
+      v.reactions().size should be(1)
       v := "Goodbye"
       v() should be("Goodbye")
       changed should be(1)
@@ -20,8 +22,13 @@ class BasicVarSpec extends WordSpec with Matchers {
     "automatically fire a change when a dependency changes" in {
       var changed = 0
       val v1 = Var("Matt")
+      v1.reactions().size should be(0)
       val v2 = Var(s"Hello, ${v1()}")
+      v1.reactions().size should be(1)
+      v2.reactions().size should be(0)
+      v2.state.references.size should be(1)
       v2.on(changed += 1)
+      v2.reactions().size should be(1)
       v2() should be("Hello, Matt")
       v1 := "Rebecca"
       v2() should be("Hello, Rebecca")
