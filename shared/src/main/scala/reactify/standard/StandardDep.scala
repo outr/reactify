fb9daa5d86182db991b3ec4a5b3ad5e9ccdd25be
Matt Hicks <matt@outr.com>
2018-07-19 10:33:05 -0500

    Simplification of StandardDep

--- shared/src/main/scala/reactify/standard/StandardDep.scala
+++ shared/src/main/scala/reactify/standard/StandardDep.scala
@@ -1,33 +1,14 @@
 package reactify.standard
 
-import java.util.concurrent.atomic.AtomicLong
-
-import reactify.{Dep, State, Var}
+import reactify.{Dep, Var}
 
 class StandardDep[T, R](override val name: Option[String],
                         override val owner: Var[R],
                         r2TFunction: R => T,
-                        t2RFunction: T => R) extends Dep[T, R] {
+                        t2RFunction: T => R) extends StandardVal[T](r2TFunction(owner), name) with Dep[T, R] {
   override def mode: Var.Mode = Var.Mode.Normal
 
-  private lazy val counter = new AtomicLong(0L)
-
-  private var _state: State[T] = new State[T](this, counter.incrementAndGet(), () => r2T(owner()))
-
-  _state.update()
-
-  override def state: State[T] = _state
-
-  override def set(value: => T): Unit = synchronized {
-    if (!state.cached.contains(value)) {
-      val previous = _state
-
-      _state = new State[T](this, counter.incrementAndGet(), () => r2T(owner()))
-      _state.update(Some(previous))
-
-      owner := t2R(value)
-    }
-  }
+  override def set(value: => T): Unit = owner := t2R(value)
 
   override def t2R(t: T): R = t2RFunction(t)
 
